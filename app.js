09ae28a25910356c36a2c50809d2c26906cb04c6
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-05-22 11:34:32 +0200

    add

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 var express = require('express');
-var app = express();
-app.all('/api/telegrambot/130906513:AAG6u4Jr8txCneVcha57SXAb9vsDbs1lINg', function (req, res) {
-  console.log(req.url)
-  res.send('OK');
+var bodyParser = require('body-parser');
+var Database = require('sequelize');
+
+var db = new Database('postgres://daniele@localhost:5432/telegram');
+var User = db.define('user', {
+  id: {
+    type: Database.INTEGER(),
+    unique: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  first_name: Database.STRING(),
+  last_name: Database.STRING(),
+  username: Database.STRING()
 });
+var PrivateChat = db.define('privateChat', {
+  id: {
+    type: Database.INTEGER(),
+    unique: true,
+    allowNull: false,
+    primaryKey: true
+  }
+});
+User.hasOne(PrivateChat);
+PrivateChat.belongsTo(User);
+User.sync();
+PrivateChat.sync();
+db.sync();
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.all('/api/telegrambot/130906513:AAG6u4Jr8txCneVcha57SXAb9vsDbs1lINg', function (req, res) {
+  if (req.body.update_id) {
+    console.log(req.body);
+    if (req.body.message.entities) {console.log(req.body.message.entities)}
+    res.send('OK');
+  } else {
+    res.send('ERROR');
+  }
+});
+
 app.listen(3000, '192.168.1.4')
